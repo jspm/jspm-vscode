c5136a0b87181fde37533149b6bbcc628def550a
Jaya Krishna <namburu1995@gmail.com>
2023-12-08 17:47:08 +0530

    fix build for extensions

--- rollup-config.mjs
+++ rollup-config.mjs
@@ -1,11 +1,11 @@
 export default {
-  input: './src/extension.mjs',
+  input: "./src/extension.mjs",
   output: {
-    file: 'dist/extension.js',
-    format: 'es',
+    file: "dist/extension.js",
+    format: "es",
     inlineDynamicImports: true,
-    sourcemap: 'inline'
+    sourcemap: "inline",
   },
-  onwarn () {},
-  plugins: []
-}
+  onwarn() {},
+  plugins: [],
+};
