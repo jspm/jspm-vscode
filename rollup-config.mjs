98dc1fa86b90139cf0fd3c13092aeb2a08c9bb48
Jaya Krishna Namburu <namburu1995@gmail.com>
2023-12-09 10:14:45 +0530

    refactor: Update the build workflow of the extensions using the inbuilt `jspm build` command. (#19)
    * Replace the existing workflow with jspm cli
    
    * fix build for extensions
    
    * remove JSPM_VS_CODE_PATH from chompfile

---
+++ rollup-config.mjs
@@ -0,0 +1,11 @@
+export default {
+  input: "./src/extension.mjs",
+  output: {
+    file: "dist/extension.js",
+    format: "es",
+    inlineDynamicImports: true,
+    sourcemap: "inline",
+  },
+  onwarn() {},
+  plugins: [],
+};
